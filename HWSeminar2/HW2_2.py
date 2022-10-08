ff9faae2775f4da9543572223ae41b35229c1051
yulia <kimkaaa@mail.ru>
2022-10-09 01:53:26 +0300

    Task2

--- HWSeminar2/HW2_2.py
+++ HWSeminar2/HW2_2.py
@@ -1,12 +1,9 @@
 # №2. Напишите программу, которая принимает на вход число num и выдает набор произведений чисел от 1 до num.
-
-# Пример:
-
 # - пусть num = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
 num = int(input("Введите целое число: "))
 num_product = 1
 result_num_product = []
 for i in range(1, num + 1):
     num_product *= i
-    result_num_product.append(num_product)  # append добавляет в список числа из списка
+    result_num_product.append(num_product) 
 print(f"{result_num_product} ")
